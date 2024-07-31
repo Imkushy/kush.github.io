a56926dab0885482677e1f2c5ffa7ecf1dda26bc
Kush <flishflashf@gmail.com>
2024-07-31 13:18:38 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,33 @@
+return {
+    Developers = {
+        Fayy = {
+            DiscordName = "6ayy.",
+            DiscordID = "855840079376285727",
+            DiscordLink = "https://discord.com/users/855840079376285727"
+        },
+        Gladiator = {
+            DiscordName = "gladiator_0315",
+            DiscordID = "1004994479482228747",
+            DiscordLink = "https://discord.com/users/1004994479482228747"
+        },
+        Blitz = {
+            DiscordName = "blitzisking",
+            DiscordID = "576849921471152140",
+            DiscordLink = "https://discord.com/users/576849921471152140"
+        }
+    },
+    DiscordInvite = "https://discord.gg/getlunara",
+    WebsiteUrl = "https://getlunara.tech",
+    Scripts = {
+        Full = {
+            Name = "Lunara",
+            Version = "1.0",
+            ChangeLog = "[+] Inital release!"
+        },
+        Lite = {
+            Name = "Lunara Lite",
+            Version = "1.1",
+            ChangeLog = "[*] Fixed support for \"solara\" executor"
+        }
+    }
+}
